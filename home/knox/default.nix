f7052c995579a6216747d2143dc6b1bb6a9d8cb8
RedType <wil.wyo@gmail.com>
2022-01-13 18:21:19 -0500

    Add knox to docker group

--- home/knox/default.nix
+++ home/knox/default.nix
@@ -3,7 +3,7 @@
 {
   users.users.knox = {
     createHome = true;
-    extraGroups = [ "audio" "networkmanager" "video" "wheel" ];
+    extraGroups = [ "audio" "docker" "networkmanager" "video" "wheel" ];
     hashedPassword =
       "$6$Uy3VXhGKzHAyxwTa$iL2Z7WjGjBHNkDfe1Xhw8zJAdTL90.a.PSzApq1vXIH2Z7aDWSwBcCJx6TnxTzhYHEROQlpfsjI24phkWBJOy0";
     isNormalUser = true;
